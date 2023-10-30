716314357548faefa1c5f2396dc6d2ed7e845c83
Renan Salustiano <renan.silva@kron.digital>
2023-10-30 05:27:52 -0300

    Feat: Informacao na home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layout')
 
 @section('content')
-    Home
+    <div class="d-flex align-items-center justify-content-center flex-column">
+        <p>Na barra de navegacao a cima é possivel gerenciar as categorias e produtos.</p>
+        <p>Basta clicar em: <a href="{{url('category')}}">Categorias</a> ou <a href="{{url('product')}}">Produtos</a> </p>
+    </div>
 @endsection
